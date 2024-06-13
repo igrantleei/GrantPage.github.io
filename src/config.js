4390d72f4479e9fcd5089560cb515f0d54723e37
igrantleei <82276976+igrantleei@users.noreply.github.com>
2024-06-13 10:51:43 +0800

    Update config.js
    。。

--- src/config.js
+++ src/config.js
@@ -1,33 +1,33 @@
 export default {
   // page1部分
   page1: {
-    titleEn: 'Hello,I`m WeiTingting', //英文标题
-    title: '一个追梦在前端路上的小白', //中文标题
+    titleEn: 'Hello,I`m Grant', //英文标题
+    title: '一个初入职场的新人', //中文标题
   },
   // page2部分
   page2: {
     authorImg: 'page1.jpg', // 作者头像
-    xinhui: '我叫卫婷婷，是一名在读的大三学生。', // 幸会
-    qiuzhi: '前端开发', // 求职意向
-    guanyuwo: '掌握了前端js、html、css基本技术和Vue框架、UI框架，nodejs等等，有着公司项目实战的经验和一些java安卓开发经历。希望可以和大家一起学习，共同进步！' // 关于我
+    xinhui: 'I'm Grant，是职场新人。', // 幸会
+    qiuzhi: '自由职业者', // 求职意向
+    guanyuwo: '周末一般有空会出门闲逛，最近在学习专业户外技能，每周去爬山。雨天喜欢在家看闲书，听有氛围感的音乐，比如爵士，萨克斯、看书看累了就锻炼一下，然后自己做一顿好吃的饭，再找一部纪录片，简直完美周末' // 关于我
   },
   // page3部分
   page3: [{
     icon: 'icon-tubiao-',
-    title: 'UI设计',
-    msg: ['PS切图、界面排版']
+    title: '户外技能',
+    msg: ['重装徒步、应急心肺复苏']
   },{
     icon: 'icon-diannao', // 图标
-    title: '网页制作', // 标题
-    msg: ['响应式页面', 'css、scss动画效果',] //介绍
+    title: '运动', // 标题
+    msg: ['健身', '游泳、漂移板',] //介绍
   }, {
     icon: 'icon-qianbi1',
-    title: '前端功能',
-    msg: ['JS完成常见的交互功能', '用AJAX读取后台数据且渲染']
+    title: '看闲书',
+    msg: ['人物传记、散文小说、']
   },  {
     icon: 'icon-shouji',
-    title: '框架',
-    msg: ['vue框架','UI框架、express框架、mysql',]
+    title: '音乐',
+    msg: ['流行摇滚、民族摇滚、民谣、萨克斯、爵士','告无人、万青、回春丹、许巍、朴树、刀郎、Lana Del Rey']
   }],
   // page4部分
   page4: {
@@ -35,35 +35,35 @@ export default {
     course: [{
       date: '2020/9——至今', // 时间
       desc: { // 经历
-        title: '山西传媒学院',
-        list: ['山西省职业技能大赛web前端开发一等奖', '专升本上岸', '软件开发公司实习']
+        title: '985',
+        list: ['东北溜达', '考研上岸', '宇宙级新能源大厂研发']
       }
     }, {
       date: '2019/9——2020/9',
       desc: {
-        title: '晋城职业技术学院',
-        list: ['学习前端语言', '构建页面 / 利用JS、CSS3等制作网站，vue有关项目', '维护508工作室','获得Web前段开发1+X证书','普通话二级甲等','国家励志奖学金']
+        title: '民办三本',
+        list: ['机械大类', '班级第一，专业前五', 'RoboMaster机器人俱乐部']
       }
     }, {
       date: '2018/9——2019/9',
       desc: {
-        title: '晋城职业技术学院',
-        list: ['学习java语言', '利用Android、sql等制作app', '参加山西省职业院校移动互联技能大赛', '全国计算机等级二级证书', '国家励志奖学金']
+        title: '-----',
+        list: ['---', '---、---', '---', '---', '--']
       }
     }],
     // 我的拓展技能掌握
     singlelist: [{
-      title: 'bootsrap',
-      text: '了解'
+      title: '吃',
+      text: '样'
     }, {
-      title: 'Element-ui',
-      text: '掌握'
+      title: '喝',
+      text: '样'
     }, {
-      title: 'mint-ui',
-      text: '掌握'
+      title: '玩',
+      text: '精'
     }, {
-      title: 'scss',
-      text: '掌握'
+      title: '乐',
+      text: '通'
     }],
     // 我的基本技能掌握
     proresslist: [{
@@ -103,7 +103,7 @@ export default {
     title: '蘑菇街',
     content: 'Vue蘑菇街商城案例前端',
     image: 'box2.png',
-    href: 'https://github.com/wttAndroid/MyShoppingStreet'
+    href: 'https://github.com'
   }, {
     title: '个人简历',
     content: 'html,css,jq,bootsrap搭建个人简历网站',
@@ -117,9 +117,9 @@ export default {
   }],
   // page6部分
   page6: {
-    github: 'https://github.com/wttAndroid',
+    github: 'https://github.com/igrantleei',
     bili: '',
-    email: 'Mailto:1457321681@qq.com?Subject=邮箱标题&Body=邮箱内容！',
+    email: 'Mailto:10086@qq.com?Subject=邮箱标题&Body=邮箱内容！',
     zhihu: '',
     weixin: "<img style='width:120px' src='http://wttandroid.gitee.io/wttandroid.github.io/src/img/weix.jpg' alt='加载失败'>",
     qq: "<img style='width:120px' src='http://wttandroid.gitee.io/wttandroid.github.io/src/img/qq.jpg' alt='加载失败'>"
